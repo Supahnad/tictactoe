d000c72569acac354cb2ca8fa5d8fbe2007f1439
Supahnad <bunanay14@gmail.com>
2022-12-15 21:28:11 +0800

    12-15-2022

--- imports/api/roomPublication.js
+++ imports/api/roomPublication.js
@@ -4,3 +4,10 @@ import { RoomsCollection } from "../db/RoomsCollection";
 Meteor.publish("rooms", function publishRooms() {
   return RoomsCollection.find({ userId: this.userId });
 });
+
+Meteor.publish("roomData", function findPlayer(playerId) {
+  return RoomsCollection.find({ player1Id: playerId });
+});
+// Meteor.publish("roomDataAll", function roomCreate(roomId) {
+//   return RoomsCollection.find({ _id: roomId });
+// });
--- imports/ui/HomePage.jsx
+++ imports/ui/HomePage.jsx
@@ -1,34 +1,47 @@
 import { Meteor } from "meteor/meteor";
-import React, { useState } from "react";
-import { Route, Routes, Navigate, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Navigate, Link, useNavigate, useParams } from "react-router-dom";
 import { useTracker } from "meteor/react-meteor-data";
 import MainNavigation from "./MainNavigation";
 import { RoomsCollection } from "../db/RoomsCollection";
 import RoomList from "./RoomList.jsx";
 
-function HomePage() {
+function HomePage(props) {
   const user = useTracker(() => Meteor.userId());
   const [roomName, setRoomName] = useState("");
+  const [roomId, setRoomId] = useState("");
+  let navigate = useNavigate();
 
+  // useEffect(() => {
+  //   console.log("props -> ", props);
+  // }, []);
 
   const logout = (e) => {
     e.preventDefault();
     Meteor.logout();
   };
 
-  const submitHandler = () => {
+  const submitHandler = (event) => {
     event.preventDefault();
     if (!roomName) return;
 
-    Meteor.call("createRoom", roomName);
-
+    Meteor.call("createRoom", roomName, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        setRoomId(res);
+        Meteor.subscribe("roomData", roomId);
+        navigate(`/TicTacToe/${res}`);
+      }
+    });
     setRoomName("");
   };
 
   return (
     <div>
       <MainNavigation />
-      {!user && <Navigate to="/LogIn" replace={true} />}
+      {/* {roomId && <Navigate to={`/TicTacToe/${roomId}`} replace={true} />} */}
+      {!user && <Navigate to="/" replace={true} />}
       <div className="room-container">
         <form className="room-form" onSubmit={submitHandler}>
           <div>
@@ -43,7 +56,11 @@ function HomePage() {
           </div>
         </form>
 
-        <button type="submit"><Link className="login-btn"  to="/RoomLists">Room lists</Link></button>
+        <button type="submit">
+          <Link className="login-btn" to="/RoomLists">
+            Room lists
+          </Link>
+        </button>
       </div>
     </div>
   );
