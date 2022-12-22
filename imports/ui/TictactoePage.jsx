e06950098049d3c11bb06dba68d6bce14524bd01
Supahnad <bunanay14@gmail.com>
2022-12-22 12:32:17 +0800

    latest update on setting winner and score

--- imports/ui/TictactoePage.jsx
+++ imports/ui/TictactoePage.jsx
@@ -21,36 +21,44 @@ function TicTacToePage() {
     return { roomData };
   });
 
-  useEffect(() => {
-    console.log("checking... ",isChecking)
-    if(isChecking === true){
-    roomData && checkForWinner(roomData.squares);
-  }
-  }, [roomData]);
-
   // const [squares, setSquares] = useState([]);
   const [turn, setTurn] = useState();
   const [winner, setWinner] = useState();
-  const [win, setWin] = useState();
   const [xscore, setxScore] = useState(0);
   const [oscore, setoScore] = useState(0);
   const [draw, setDraw] = useState(false);
   const [isWaiting, setIsWaiting] = useState(true);
   const [Player2, setPlayer2] = useState(null);
 
-  const checkForPlayer = () => {
-      if (roomData.player2Id !== null) {
-        // console.log("Ready to Play ");
-        setPlayer2(roomData.player2Id);
-        setIsWaiting(false);
-      } else {
-        // console.log("waiting for Opponent ");
-        setIsWaiting(true);
+
+  useEffect(() => {
+    if (roomData) {
+      // console.log("checking... ", isChecking);
+      if(isChecking){
+        console.log("roomoData ==>", roomData.squares);
+        console.log("checkForWinner function returns  ",checkForWinner(roomData.squares));
+        checkForWinner(roomData.squares)
       }
+      checkForPlayer();
+      setxScore(roomData.xScore);
+      setoScore(roomData.oScore);
+    } else {
+      console.log("waiting for roomoData..");
+    }
+  }, [roomData]);
+
+  const checkForPlayer = () => {
+    if (roomData.player2Id !== null) {
+      // console.log("Ready to Play ");
+      setPlayer2(roomData.player2Id);
+      setIsWaiting(false);
+    } else {
+      // console.log("waiting for Opponent ");
+      setIsWaiting(true);
+    }
   };
 
   const checkForWinner = (square, index) => {
-    console.log("2nd checking... ",isChecking)
     let patterns = [
       [0, 1, 2],
       [3, 4, 5],
@@ -79,34 +87,38 @@ function TicTacToePage() {
       ) {
         setWinner(square[a]);
         if (square[a] === "x") {
-          Meteor.call("set.Score", roomId)
-          console.log("player 1 wins")
+          // Meteor.call("set.Score", roomId);
+          console.log("player 1 wins");
+          return true;
         } else if (square[a] === "o") {
-          console.log("player 2 wins")
+          console.log("player 2 wins");
           // setoScore(oscore + 1);
         }
       }
 
-      if (boxFull === true) {
-        if (square[a] && square[a] === square[b] && square[b] === square[c]) {
-          setWinner(square[a]);
-          if (square[a] === "x") {
-            // setxScore(xscore + 1);
-          } else if (square[a] === "o") {
-            // setoScore(oscore + 1);
-          }
-        }
-        if (square[a] !== square[b] && square[a] !== square[c] && !winner) {
-          setDraw(true);
-        }
-      }
+      // if (boxFull === true) {
+      //   if (square[a] && square[a] === square[b] && square[b] === square[c]) {
+      //     setWinner(square[a]);
+      //     if (square[a] === "x") {
+      //       // setxScore(xscore + 1);
+      //     } else if (square[a] === "o") {
+      //       // setoScore(oscore + 1);
+      //     }
+      //   }
+      //   if (square[a] !== square[b] && square[a] !== square[c] && !winner) {
+      //     setDraw(true);
+      //   }
+      // }
     }
     setIsChecking(false);
-    console.log("3rd checking... ",isChecking)
+    // console.log("checking after setting to false ", isChecking);
+    return false;
   };
 
   const ClickHandler = (index) => {
+
     setIsChecking(true);
+    
     Meteor.call("place.move", roomId, index, roomData.squares, (err, res) => {
       if (res.status === "success") {
         // console.log("response ",res.response)
@@ -117,6 +129,7 @@ function TicTacToePage() {
   };
 
   const restartHandler = () => {
+    Meteor.call("reset.Game", roomId);
     setWinner(null);
     setTurn("x");
     setDraw(false);
@@ -128,14 +141,6 @@ function TicTacToePage() {
     }
     navigate(`/Home`);
   };
-
-  useEffect(() => {
-    if (roomData) {
-    checkForPlayer();
-    setxScore(roomData.xScore);
-    setoScore(roomData.oScore);
-    }
-  }, [roomData]);
 
   return (
     <>
@@ -153,10 +158,20 @@ function TicTacToePage() {
       ) : (
         <div className="game-container">
           <h1>Let's Play TicTacToe</h1>
-          <h2>player {roomData.turn === roomData.player1Id ? roomData.player1Username : roomData.player2Username }'s turn</h2>
+          <h2>
+            player{" "}
+            {roomData.turn === roomData.player1Id
+              ? roomData.player1Username
+              : roomData.player2Username}
+            's turn
+          </h2>
           <div className="Scoreboard">
-            <span>{roomData.player1Username}: {xscore}</span>
-            <span>{roomData.player2Username}: {oscore}</span>
+            <span>
+              {roomData.player1Username}: {xscore}
+            </span>
+            <span>
+              {roomData.player2Username}: {oscore}
+            </span>
           </div>
           {/* created a new array of Squares Component
          using the map() and the created array earlier ---> const [squares, setSquares] = useState(new Array(9).fill(null));*/}
